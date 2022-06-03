7e52272b579545b3b7a3fa66a6821a54dff98f28
Romain Lenoir <romain-l58@hotmail.fr>
2022-06-03 10:17:06 +0200

    fix bug server down

--- back/index.js
+++ back/index.js
@@ -40,47 +40,44 @@ io.on('connection', (socket) => {
 
 // envoyez l'état du jeu à tous les joueurs connectés, 60 fois par secondes 
 function update() {
-    //console.log("heros = ", heros)
     io.volatile.emit('hero list', Object.values(heros)); //Le flag volatile signifie que le paquet de données peut être perdu (à cause d'une latence, d'une déconnexion, etc...).
 }
 
-setInterval(update, 1000 / 60);
+setInterval(update, 1000 / 25); //1000 = 1sec
 
 io.on('connection', function (socket) {
     socket.on('init hero', function (myhero) {
-        if (heros != {}) {
-            heros[socket.id] = myhero
-            heros[socket.id].hero.id = socket.id
-        }
+        heros[socket.id] = myhero
+        heros[socket.id].hero.id = socket.id
     });
 
     socket.on('move hero', function (position) {
-        if (heros != {}) {
+        if (heros[socket.id] != null) {
             heros[socket.id].hero.x = position.x
             heros[socket.id].hero.y = position.y
         }
     });
 
     socket.on('Change hero direction', function (data) {
-        if (heros != {}) {
+        if (heros[socket.id] != null) {
             heros[socket.id].hero.direction = data.direction
         }
     });
 
     socket.on('Change hero last direction', function (data) {
-        if (heros != {}) {
+        if (heros[socket.id] != null) {
             heros[socket.id].hero.lastDirection = data.lastDirection
         }
     });
 
     socket.on('Move state', function (data) {
-        if (heros != {}) {
+        if (heros[socket.id] != null) {
             heros[socket.id].hero.state = data.state
         }
     });
 
     socket.on('Change map', function (data) {
-        if (heros != {}) {
+        if (heros[socket.id] != null) {
             heros[socket.id].hero.map = data.map
         }
     });
